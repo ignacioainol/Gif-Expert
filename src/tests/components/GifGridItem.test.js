8fd440479a1fa3635ecdb3956529f98c079bb3ee
Ignacio <shoik.xtazy@gmail.com>
2020-12-15 23:51:18 -0300

    test unitarios GifGridItem

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,8 @@
+import { shallow } from 'enzyme';
+
+describe('Pruebas en <GifGridItem />', () => {
+    test('debe de mostrar el componente correctamente ', () => {
+        const wrapper = shallow
+    })
+
+})
