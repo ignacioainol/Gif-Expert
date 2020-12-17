5589f4f5a89ab1798186aeaaa77b16e80330a6f1
Ignacio <shoik.xtazy@gmail.com>
2020-12-16 22:15:37 -0300

    more test in GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -1,8 +1,36 @@
+import React from 'react';
 import { shallow } from 'enzyme';
+import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
+
+    const title = "Some Title";
+    const url = 'https://localhost/image.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
     test('debe de mostrar el componente correctamente ', () => {
-        const wrapper = shallow
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe de tener un parrafo con el title', () => {
+        const paraph = wrapper.find('p');
+        expect(paraph.text().trim()).toBe(title);
     })
+
+    test('debe de tener la imagen igual al url y alt de los props ', () => {
+        const img = wrapper.find('img');
+        // console.log(img.props());
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('src')).toBe(url);
+    });
+
+    test('Debe de tener animate__fadeIn ', () => {
+        const div = wrapper.find('div');
+        // console.log(div.prop('className').includes('animate__fadeIn'));
+        expect(div.prop('className').includes('animate__fadeIn')).toBe(true);
+    })
+
+
+
 
 })
