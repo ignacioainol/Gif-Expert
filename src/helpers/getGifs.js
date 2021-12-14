43493d1dd3664e26969c781a30b60d9b782931e7
Ignacio Ainol <iggnaxios@gmail.com>
2021-12-14 03:23:54 -0300

    actualizando api key giphy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=I1gDz2zBvvBCzuc03yQRgjDvoRBKNaLk`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=Ap2P2VCyb8Hj8KpF6OOe8NNxQ9S0G6tU`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
